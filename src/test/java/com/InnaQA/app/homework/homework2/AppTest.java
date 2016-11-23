25a9fd9846fe36684fd588283a5aa03e399f5fa7
Inna <inna.piddema@sannacode.com>
2016-11-23 23:57:17 +0200

    Commit_number_two_23_11_2016

--- src/test/java/com/InnaQA/app/homework/homework2/AppTest.java
+++ src/test/java/com/InnaQA/app/homework/homework2/AppTest.java
@@ -10,9 +10,22 @@ public class AppTest {
 
     @Test
     @FileParameters(value = "src/test/resources/homework2/test1.csv")
-    public void parametrizedTest(int x, int y, int result) {
+    public void Test1(int x, int y, int result) {
         assert x * y == result;
     }
 
-    
+    @Test
+    @FileParameters(value = "src/test/resources/homework2/test2.csv")
+    public void Test2(int x, int y, int result) {
+        assert x / y == result;
+        System.out.println("Tests finished ! You result is:  " + result);
+    }
+
+    @Test
+    @FileParameters(value = "src/test/resources/homework2/test3.csv")
+    public void Test3(int x, int y,int q, int result) {
+        assert (x / y) + q == result;
+        System.out.println("Tests finished ! You result is:  " + result);
+    }
+
 }
