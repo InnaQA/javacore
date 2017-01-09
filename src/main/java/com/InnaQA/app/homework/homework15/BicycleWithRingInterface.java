b0402fea3dda76faa3f3576e3f2d2864a13b9892
InnaQA <innapiddema2013@mail.ru>
2017-01-10 01:31:13 +0200

    Update BicycleWithRingInterface.java

--- src/main/java/com/InnaQA/app/homework/homework15/BicycleWithRingInterface.java
+++ src/main/java/com/InnaQA/app/homework/homework15/BicycleWithRingInterface.java
@@ -1,7 +1,5 @@
 package com.InnaQA.app.homework.homework15;
 
-/**
- * Created by Інна on 09.01.2017.
- */
-public class BicycleWithRingInterface {
+public interface BicycleWithRingInterface {
+    void ring();
 }
