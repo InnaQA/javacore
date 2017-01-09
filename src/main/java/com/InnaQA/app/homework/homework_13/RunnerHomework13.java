9df433ada68d302600cc657fa0e4fa20782643d1
InnaQA <innapiddema2013@mail.ru>
2017-01-10 01:14:12 +0200

    Update RunnerHomework13.java

--- src/main/java/com/InnaQA/app/homework/homework_13/RunnerHomework13.java
+++ src/main/java/com/InnaQA/app/homework/homework_13/RunnerHomework13.java
@@ -4,4 +4,28 @@ package com.InnaQA.app.homework.homework_13;
  * Created by Інна on 10.01.2017.
  */
 public class RunnerHomework13 {
+  import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class RunnerHomework13 {
+    public static void main(String[] args) {
+        System.out.println("Homework13 ");
+        //если ввести возраст, что не входит в диапазон 1 - 120 - высветится Age invalid.
+        WhiteCollar Runner = new WhiteCollar("Inna", 20, "Sannacode"); // если ввести Саннакод вместо Sannacode - высветится Company name is invalid
+        Scanner scanner = new Scanner(System.in);
+        String runner = scanner.nextLine();
+        int menu = parseInt(runner);
+        switch (menu) {
+            case 1:
+                Runner.getName();
+                break;
+            case 2:
+                Runner.getAge();
+                break;
+            case 3:
+                Runner.getCompany();
+                break;
+        }
+    }
 }
