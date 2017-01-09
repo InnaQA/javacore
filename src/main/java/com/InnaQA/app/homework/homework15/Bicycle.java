6718ce4f28c01e841bb2b27c169f5fec733654ca
InnaQA <innapiddema2013@mail.ru>
2017-01-10 01:30:22 +0200

    Update Bicycle.java

--- src/main/java/com/InnaQA/app/homework/homework15/Bicycle.java
+++ src/main/java/com/InnaQA/app/homework/homework15/Bicycle.java
@@ -1,7 +1,14 @@
 package com.InnaQA.app.homework.homework15;
 
-/**
- * Created by Інна on 09.01.2017.
- */
-public class Bicycle {
+public abstract class Bicycle {
+    public abstract void ride();
+
+    public abstract int setSpeed(int speed);
+
+    public abstract int setGear(int gear);
+
+    {
+        System.out.println("Wroom!");
+    }
 }
+
