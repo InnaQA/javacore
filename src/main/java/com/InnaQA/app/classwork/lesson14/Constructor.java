4bfd3ca91ba6289c1f776a15ee09645a5a9289b0
Inna <inna.piddema@sannacode.com>
2017-01-09 22:16:31 +0200

    Commit_homework13

---
+++ src/main/java/com/InnaQA/app/classwork/lesson14/Constructor.java
@@ -0,0 +1,7 @@
+package com.InnaQA.app.classwork.lesson14;
+
+/**
+ * Created by Інна on 15.12.2016.
+ */
+public class Constructor {
+}
---
+++ src/main/java/com/InnaQA/app/classwork/lesson15/Student15.java
@@ -0,0 +1,7 @@
+package com.InnaQA.app.classwork.lesson15;
+
+/**
+ * Created by Інна on 20.12.2016.
+ */
+public class Student15 {
+}
