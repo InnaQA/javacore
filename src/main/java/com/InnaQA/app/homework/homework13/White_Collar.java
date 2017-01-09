8b163ba2abf92cb9c12e16d24ce8b818758974a7
InnaQA <innapiddema2013@mail.ru>
2017-01-10 01:18:26 +0200

    Update White_Collar.java

--- src/main/java/com/InnaQA/app/homework/homework13/White_Collar.java
+++ src/main/java/com/InnaQA/app/homework/homework13/White_Collar.java
@@ -1,7 +1,38 @@
 package com.InnaQA.app.homework.homework13;
 
-/**
- * Created by Інна on 10.01.2017.
- */
-public class White_Collar {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WhiteCollar extends com.InnaQA.app.homework.homework_13.Human {
+    private String company;
+
+
+    public WhiteCollar(String name, int age, String company) {
+        super(name, age);
+        if (checkCompanyName(company)) {
+            this.company = company;
+        }
+    }
+
+    public boolean checkCompanyName(String company) {
+        Pattern pattern = Pattern.compile("([a-zA-Z| |,-])*");
+        Matcher matcher = pattern.matcher(company);
+        String result = (matcher.matches()) ? "Name of company: " + company : "Company name is invalid";
+        System.out.println(result);
+
+        return false;
+
+    }
+    public String getCompany() {
+        return company;
+    }
+
+
+    public void setCompany(String company) {
+        if (checkCompanyName(company)) {
+            this.company = company;
+        }
+
+    }
+
 }
