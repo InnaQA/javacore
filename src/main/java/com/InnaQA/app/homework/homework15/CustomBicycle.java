1ba0f047844295b168a2948fce89123c5cc78936
InnaQA <innapiddema2013@mail.ru>
2017-01-10 01:31:39 +0200

    Update CustomBicycle.java

--- src/main/java/com/InnaQA/app/homework/homework15/CustomBicycle.java
+++ src/main/java/com/InnaQA/app/homework/homework15/CustomBicycle.java
@@ -1,7 +1,27 @@
 package com.InnaQA.app.homework.homework15;
 
-/**
- * Created by Інна on 09.01.2017.
- */
-public class CustomBicycle {
+public class CustomBicycle  extends Bicycle  {
+    @Override
+    public void ride() {
+        ride();
+        System.out.println("Wshhhh!");
+    }
+
+    @Override
+    public int setSpeed(int speed) {
+        return speed;
+
+    }
+
+    @Override
+    public int setGear(int gear) {
+        return gear;
+
+    }
+    public void printSpeed (int speed){
+        System.out.println("Speed: "+ speed);
+    }
+    public void printGear (int gear){
+        System.out.println("Gear: " + gear);
+    }
 }
